1ddd1796185f9ee016b4fab9db775924bd28029c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-05-22 23:54:23 +0000

    User

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -2,31 +2,32 @@
 """ 
 User class
 """
-
-class User():
-    """ Documentation """
-
-    def __init__(self, email):
-        """ Documentation """
-        if type(email) is not str:
-            raise TypeError("email must be a string")
-        else:
-            self.__email = email
-
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
-
-
 if __name__ == "__main__":
+
+    class User():
+        """ Documentation """
+
+        def __init__(self, email):
+            """ Documentation """
+            if type(email) is not str:
+                raise TypeError("email must be a string")
+            else:
+                self.__email = email
+
+        @property
+        def email(self):
+            """ Documentation """
+            return self.__email
+
+        @email.setter
+        def email(self, value):
+            """ Documentation """
+            if type(value) is not str:
+                raise TypeError("email must be a string")
+            self.__email = value
+
+
+#if __name__ == "__main__":
 
     u = User("john@snow.com")
    # u.email = "john@snow.com"
